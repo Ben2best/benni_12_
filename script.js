e9c160cc549fcd999032f45f323ff86355931ded
Ben2best <b.heumader2012@gmail.com>
2026-08-02 18:45:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,27 @@ navigator.clipboard.writeText("benni_12_");
 alert("Discord copied!");
 
 }
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const current = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(current < target) {
+            counter.innerText = Math.ceil(current + increment);
+            setTimeout(updateCounter, 20);
+        } else {
+            counter.innerText = target + "+";
+        }
+
+    };
+
+    updateCounter();
+
+});
